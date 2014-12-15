7696a88b4bca7854b7fe60d2fc6db64f3a4227c9
PJ King <github@pjking.net>
2014-12-15 10:22:00 -0500

    devbuild no longer forces sudo

--- devbuild.sh
+++ devbuild.sh
@@ -1,2 +1,8 @@
-sudo bundle update &&
+#!/bin/bash
+if [[ $EUID -eq 0 ]]
+then
+    bundle update
+else
+    echo "Run script as sudo in order to update jekyll \n"
+fi
 bundle exec jekyll build --watch --drafts --config _config.yml,_config-dev.yml
