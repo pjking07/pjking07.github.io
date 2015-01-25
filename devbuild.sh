4fc81c4a8ac9ace02ea08a7ad9484b710fe7ca87
PJ King <github@pjking.net>
2015-01-25 13:01:40 -0500

    Changed sudo language in devbuild.sh

--- devbuild.sh
+++ devbuild.sh
@@ -3,6 +3,6 @@ if [[ $EUID -eq 0 ]]
 then
     bundle update
 else
-    echo "Run script as sudo in order to update jekyll \n"
+    echo "Update jekyll by running script as sudo\n"
 fi
 bundle exec jekyll build --watch --drafts --config _config.yml,_config-dev.yml
